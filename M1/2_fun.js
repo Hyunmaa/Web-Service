035c7b2f300566103365c0eb8257da27f03945bb
Hyuuuunma <90091717+Hyunmaa@users.noreply.github.com>
2024-02-07 10:31:07 +0700

    Create 2_fun.js

---
+++ M1/2_fun.js
@@ -0,0 +1,7 @@
+function panggilNamaku(nama, umur) {
+  console.log(`${nama} berumur ${umur}`);
+}
+
+const panggilNamakuBaru = function (nama, umur) {
+  console.log(`${nama} berumur ${umur}`);
+};
